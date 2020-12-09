5345a3de3b62c693cb3af6d0307119aabdcd6e19
charlesikulayo <sleektrends7@gmail.com>
2020-12-09 14:49:16 +0100

    Cleaned up extra spaces

--- Accounting/src/Transaction.java
+++ Accounting/src/Transaction.java
@@ -1,12 +1,35 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Transaction {
-    private double accountBalance;
-    private ArrayList<Double> depositAmount = new ArrayList<>();
-    private ArrayList<Double> withdrawAmount = new ArrayList<>();
+public class Transaction  {
+    protected double accountBalance;
+    private final ArrayList<Double> depositAmount = new ArrayList<>();
+    private final ArrayList<Double> withdrawAmount = new ArrayList<>();
+
+    private final Map<String,Double> transferIn = new HashMap<>();
+    private final Map<String,Double> transferOut = new HashMap<>();
+
 
     public Transaction() {
     }
+
+    //Getters
+    public Map<String, Double> getTransferOut() {
+        return transferOut;
+    }
+
+    public ArrayList<Double> getDepositAmount() {
+        return depositAmount;
+    }
+
+    public ArrayList<Double> getWithdrawAmount() {
+        return withdrawAmount;
+    }
+    public double getAccountBalance (){
+        return accountBalance;
+    }
+    //METHODS
 
     //method to deposit
     public double deposit (double amount){
@@ -21,12 +44,17 @@ public class Transaction {
         withdrawAmount.add(amount);
         return accountBalance;
     }
+    public double transferIn (String description ,double amount ) {
+        transferIn.put(description,amount);
+        accountBalance += amount;
+        System.out.println(" description " + description);
+        return accountBalance;
 
-    public ArrayList<Double> getDepositAmount() {
-        return depositAmount;
     }
 
-    public ArrayList<Double> getWithdrawAmount() {
-        return withdrawAmount;
+    public double transferOut (String description, double amount){
+        transferOut.put(description,amount);
+        accountBalance -= amount;
+        return accountBalance;
     }
 }
